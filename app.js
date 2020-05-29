d8ecd1ba3ad46c06e021c9d1618473321e068677
Taras-prog <tarascr7@gmail.com>
2020-05-29 20:25:34 +0300

    commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const os = require('os')
 const fs = require('fs');
+// const fs = require('fs-extra')
 
 // const {calculate} = require('./dir1/mine');
 
@@ -8,7 +9,14 @@ const fs = require('fs');
 
 // console.log(number);
 
-//-------------------fs--------------------
+// console.log(os.platform()); // Перевіряє платформу (Windows, Mac, Linux)
+
+// console.log(process.cwd()); // cwd = current work directory 
+// console.log(__dirname ); // імя папки 
+// console.log(__filename); // iмя файла
+
+
+//===============================fs========================================
 // fs.writeFile(
 //     path.join(__dirname, 'files', 'data.txt'),
 //     'hello world \n',
@@ -24,31 +32,49 @@ const fs = require('fs');
 //         {flag: 'a'}, 
 //         err => {
 //         console.log(err);
-        
 //     })
-
 // }
 
-// fs.readFile(path.join(__dirname, 'files', 'data.txt'), (err, data)  => {
+// fs.readFile(path.join(__dirname, 'files', 'data.txt'), (err, data)  => {  //  зчитує  файл
 //     console.log(data.toString());
 // });
 
-fs.stat(path.join(__dirname, 'files', 'data.txt'), (err, stats) => {
-     console.log(stats);
-     
-     
-}
-)
-//------------------------------------------------------
+// fs.stat(path.join(__dirname, 'files', 'data.txt'), (err, stats) => { // показує статус 
+//      console.log(stats);     
+// });
+
+// fs.mkdir(path.join(__dirname, 'first node'), err => { // створює папку 
+//      console.log(err);
+// });
+
+// fs.rmdir(path.join(__dirname, 'first node'), err => { // видаляє папку
+//      console.log(err);
+// });
+
+// fs.readdir(path.join(__dirname, 'first node'), (err, files) => {
+//      console.log(files);
+// });
+
+// fs.stat(path.join(__dirname, 'first node'), (err, stats)=>{
+//      console.log(stats.isDirectory());
+// });
+
+// fs.unlink(path.join(__dirname, 'app.txt'), err => { // видаляє файл
+//      console.log(err);
+// });
+
+// fs.rename(path.join(__dirname, 'first.txt'), path.join(__dirname, 'files', 'first.txt'), err=> {
+//      console.log(err);  
+// }); // змінює переіменовує та переміщає файли
+
+// fs.truncate(path.join(__dirname, 'hello.txt'), 3, err => {
+//      console.log(err);   
+// }); // чистить файл
+//===============================================================================
 
 
-// console.log(os.platform()); // Перевіряє платформу (Windows, Mac, Linux)
 
-// console.log(process.cwd()); // cwd = current work directory 
-// console.log(__dirname ); // імя папки 
-// console.log(__filename); // iмя файла
-
-// -------------------------------------PATH--------------------------------------------------
+// ******************************************PATH******************************************
 // let s = path.resolve(`${process.cwd()}\\\\..\\/React`); // виправляє зломаний шлях до папки
 
 // console.log(s);
@@ -56,4 +82,4 @@ fs.stat(path.join(__dirname, 'files', 'data.txt'), (err, stats) => {
 // let s1 = path.join(__dirname, 'files', 'data.txt');
 
 // console.log(s1);
-//--------------------------------------------------------------------------------------------
+//******************************************************************************************
