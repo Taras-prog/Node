9a80a14ee0a638c1d4e0704296f35e9f71853014
Taras-prog <tarascr7@gmail.com>
2020-06-02 20:10:46 +0300

    commit

--- Lesson-2/app.js
+++ Lesson-2/app.js
@@ -20,7 +20,44 @@
 // console.log(ee.listenerCount('hello'));
 
 
+// const fs = require('fs');
 
+// let writeStream = fs.createWriteStream('./video_copy.mp4');
 
+// // for (let i = 0; i < 1000; i++) {
+// //     writeStream.write('17 contributions in the last year ')
+// // };
+
+// let readStream = fs.createReadStream('./video.mp4');
+
+// readStream.on('data', (chunk) => {
+//     console.log(chunk.toString());
+//     console.log('__________');
+// });
+ 
+// readStream.on('end', () => {
+//     console.log('End reading!');    
+// });
+
+// readStream.pipe(writeStream);
 
 // https://github.com/Taras-prog/Node.git
+
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log(req);
+    
+});
+
+
+app.listen(7777, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Listen 7777...');
+    }
+});
