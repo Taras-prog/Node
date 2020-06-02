471becfeaf719832d3273aac92d8c950776d5e0b
Taras-prog <tarascr7@gmail.com>
2020-06-02 17:24:30 +0300

    https://github.com/Taras-prog/Node.git

---
+++ Lesson-2/app.js
@@ -0,0 +1,26 @@
+// const {EventEmitter} = require('events');
+
+// const ee = new EventEmitter;
+
+// ee.on('hello', () => {
+//     console.log('HELLO WORLD');
+// }); //можно запускати багато разів
+
+
+// ee.once('test', (firstArg, secondArg) => {
+//     console.log('Test Event')
+//     console.log(firstArg)
+//     console.log(secondArg)
+// }); //можна запустити тільки один раз 
+
+// ee.emit('hello');
+// ee.emit('test', 'one', 2)
+
+// console.log(ee.listenerCount('test'));
+// console.log(ee.listenerCount('hello'));
+
+
+
+
+
+// https://github.com/Taras-prog/Node.git
