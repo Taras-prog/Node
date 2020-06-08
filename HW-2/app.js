c3c7938f9612504a0aab35affdf72764e6808414
Taras-prog <tarascr7@gmail.com>
2020-06-08 19:50:28 +0300

    commit

--- HW-2/app.js
+++ HW-2/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressBars = require('express-handlebars');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
@@ -58,8 +59,40 @@ app.post('/register', (req, res) => {
     }
 });
 
+app.get('/video', function(req, res) {
+    const path = 'some/video.mp4'
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-")
+      const start = parseInt(parts[0], 10)
+      const end = parts[1] 
+        ? parseInt(parts[1], 10)
+        : fileSize-1
+      const chunksize = (end-start)+1
+      const file = fs.createReadStream(path, {start, end})
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(200, head)
+      fs.createReadStream(path).pipe(res)
+    }
+  });
+
 app.use((req, res) => {
     res.status(404).render('404')
 });
 
-app.listen(3001, () => console.log('Listen 3001...'));
+
+app.listen(4000, () => console.log('Listen 4000...'));
