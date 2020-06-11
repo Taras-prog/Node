c5a914bd5786ff7b150d097cfe97f82b88e7a36b
Taras-prog <tarascr7@gmail.com>
2020-06-11 21:35:56 +0300

    delete commit

--- HW-3/controllers/404/404.controller.js
+++
@@ -1,1 +0,0 @@
-module.exports = (req, res) => res.status(404).json({page: 404})
--- HW-3/controllers/index.js
+++
@@ -1,3 +0,0 @@
-module.exports.userController = require('./user/user.controller');
-module.exports.productController = require('./product/product.controller');
-module.exports.notFoundController = require('./404/404.controller');
