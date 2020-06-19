6893bec3a2bc6ea0c947f050ab09ac983c8db506
Taras-prog <tarascr7@gmail.com>
2020-06-19 20:54:05 +0300

    commit

--- Lesson-4/dataBase/index.js
+++ Lesson-4/dataBase/index.js
@@ -1,11 +1,41 @@
-const mysql2 = require('mysql2');
+// const mysql2 = require('mysql2');
 
-const connection = mysql2.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'new_schema',
-    password: 'tarascr777!'
-  });
+// const connection = mysql2.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'new_schema',
+//     password: 'tarascr777!'
+//   });
 
 
-module.exports = connection;
+// module.exports = connection;
+
+const Sequelize = require('sequelize');
+
+
+function initConnection() {
+    const client = new Sequelize('new_schema', 'root', 'tarascr777!', {
+      host: 'localhost',
+      dialect: 'mysql'
+    })
+      let models = {};
+
+      
+
+}
+
+
+module.exports = (()=>{
+  let instanse;
+
+
+  return {
+    getInstanse: () => {
+      if (!instanse) {
+         instanse = 'xxx'
+      }
+      return instanse;
+    }
+  }
+})();
+
